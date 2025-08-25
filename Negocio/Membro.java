bc5c55f7394230a0e7694d2804003b5bcd133f34
paesgabryel@gmail.com <paesgabryel@gmail.com>
2025-08-25 16:39:34 -0300

    third commit

---
+++ Negocio/Membro.java
@@ -0,0 +1,23 @@
+package Negocio;
+
+public class Membro extends Pessoa {
+
+    private int emprestimosAtivos;
+    private String historicoEmprestimos;
+
+    public void consultarEmprestimos() {
+
+    }
+
+    public void consultarHistorico() {
+
+    }
+
+    public void debitosPendentes() {
+
+    }
+
+    public void calcularMultas() {
+
+    }
+}
